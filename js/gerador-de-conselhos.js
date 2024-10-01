f9e8830422d0401862e80571854fc7b996d938c7
Matheus Compagnoni <compagnoni_santos@hotmail.com>
2024-10-01 19:49:07 -0300

    finalizando o javascript do projeto dos conselhos aleatorios

--- js/gerador-de-conselhos.js
+++ js/gerador-de-conselhos.js
@@ -1,17 +1,25 @@
 const botaoDeConselhos = document.querySelector('.botao-do-conselho');
 const conselhoId = document.querySelector('.id-do-conselho');
-const descricaoDoConselho = document.querySelector('.descricao-do-conselho');
+const conselhoDescricao = document.querySelector('.descricao-do-conselho');
 
 async function conselhoAleatorioDaApi() {
 
-      const url = "https://api.adviceslip.com/advice"
-      const resposta = await fetch(url)
-      const jsonDoConselho = await resposta.json()
-      const idDoConselho = jsonDoConselho.slip.id
-      const descricaoDoConselho = jsonDoConselho.slip.advice
-      
+      const url = "https://api.adviceslip.com/advice";
+      const resposta = await fetch(url);
+      const jsonDoConselho = await resposta.json();
+      const idDoConselho = `Advice #${jsonDoConselho.slip.id}`;
+      const descricaoDoConselho = `"${jsonDoConselho.slip.advice}"`;
 
+      conselhoId.innerText = idDoConselho;
+      conselhoDescricao.innerText = descricaoDoConselho
+   
 }
 
-
 conselhoAleatorioDaApi()
+
+botaoDeConselhos.addEventListener("click", function(){
+   conselhoAleatorioDaApi()
+})
+
+
+
